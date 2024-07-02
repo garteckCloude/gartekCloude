6f15e664238f66a0ea3b61bd5fb61d168a44abd3
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-02 06:44:29 +0100

    Add regular expression for email validation
    Add input validation for the first name field
    Add input validation for the first name field

--- contact-section/main.js
+++ contact-section/main.js
@@ -5,3 +5,35 @@ const messageLastNameElement = messageForm.elements['last-name'];
 const messageFormEmailAddress = messageForm.elements['email-address'];
 const messageFormComment = messageForm.elements['comment'];
 const messageFormSubmitBtn = document.querySelector('.message-form-submit-btn');
+
+// Regular expression for validating email addresses
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+// Adding input validation for the first name field
+messageFirstNameElement.addEventListener('input', (e) => {
+  let value = messageFirstNameElement.value;
+
+  if (value.trim() === '') {
+    messageFirstNameElement.setCustomValidity('Please input your first name');
+  } else {
+    messageFirstNameElement.setCustomValidity('');
+  }
+
+  messageFirstNameElement.reportValidity();
+});
+
+// Adding input validation for the email address field
+messageFormEmailAddress.addEventListener('input', (e) => {
+  let value = messageFormEmailAddress.value;
+
+  if (value.trim() === '' || !emailRegExp.test(value.trim())) {
+    messageFormEmailAddress.setCustomValidity(
+      'Please input a valid email address',
+    );
+  } else {
+    messageFormEmailAddress.setCustomValidity('');
+  }
+
+  messageFormEmailAddress.reportValidity();
+});
