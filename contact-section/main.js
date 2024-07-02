db331f12b456063ba088babe1e09222425354b1f
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-02 06:42:55 +0100

    Query form and elements for message form handling

---
+++ contact-section/main.js
@@ -0,0 +1,7 @@
+// Querying DOM elements for the message form and its inputs
+const messageForm = document.querySelector('.message-form');
+const messageFirstNameElement = messageForm.elements['first-name'];
+const messageLastNameElement = messageForm.elements['last-name'];
+const messageFormEmailAddress = messageForm.elements['email-address'];
+const messageFormComment = messageForm.elements['comment'];
+const messageFormSubmitBtn = document.querySelector('.message-form-submit-btn');
