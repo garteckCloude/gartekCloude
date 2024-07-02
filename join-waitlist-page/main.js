bd9636215d3fb65607ac4fba93d2c5ba973f00d2
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-02 17:27:13 +0100

    feat: initial commit for DOM element queries
    - Query DOM elements for the off-screen menu toggle button
    - Query DOM elements for the close button

---
+++ join-waitlist-page/main.js
@@ -0,0 +1,4 @@
+// Querying DOM elements for the off-screen menu toggle button and close button
+const toggleBtn = document.querySelector('.menu-toggle-btn');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+const closeOffScreenMenu = document.querySelector('.close-off-screen-menu-btn');
