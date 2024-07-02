8bfa748da3a6073386e50daa7cc5e6b522b30a9c
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-02 17:27:36 +0100

    feat: add event listener for menu toggle button
    - Add event listener to toggle off-screen menu visibility
    - Implement functionality to show and hide the off-screen menu

--- join-waitlist-page/main.js
+++ join-waitlist-page/main.js
@@ -2,3 +2,21 @@
 const toggleBtn = document.querySelector('.menu-toggle-btn');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 const closeOffScreenMenu = document.querySelector('.close-off-screen-menu-btn');
+
+// Toggle off-screen menu visibility when the toggle button is clicked
+toggleBtn.addEventListener('click', (e) => {
+  const { currentTarget } = e;
+  const { menuState } = currentTarget.dataset;
+
+  if (menuState === 'hidden') {
+    offScreenMenu.dataset.menuState = 'visible';
+    closeOffScreenMenu.dataset.menuState = 'visible';
+    toggleBtn.dataset.menuState = 'visible';
+  }
+
+  if (menuState === 'visible') {
+    offScreenMenu.dataset.menuState = 'hidden';
+    closeOffScreenMenu.dataset.menuState = 'hidden';
+    toggleBtn.dataset.menuState = 'hidden';
+  }
+});
