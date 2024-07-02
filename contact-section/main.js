827e7fa2c13f14c63308fc7866edc9930b44a97f
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-02 06:46:34 +0100

    Query DOM for off-screen menu and its elements

--- contact-section/main.js
+++ contact-section/main.js
@@ -64,3 +64,8 @@ messageForm.addEventListener('submit', (e) => {
   // Perform action in the backend with the form values
   alert('form submitted');
 });
+
+// Querying DOM elements for the off-screen menu toggle button and close button
+const toggleBtn = document.querySelector('.menu-toggle-btn');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+const closeOffScreenMenu = document.querySelector('.close-off-screen-menu-btn');
