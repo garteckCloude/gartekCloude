2f5b72757b7c54337283e33d87a74a48d3d57f17
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-07-14 11:43:52 +0100

    feat: add email validation and send post req

--- frontend/join-waitlist-page/main.js
+++ frontend/join-waitlist-page/main.js
@@ -1,3 +1,60 @@
+const joinWaitListFormEl = document.querySelector('.join-waitlist-form');
+const joinWaitListEmailAddressEl = joinWaitListFormEl.elements['email'];
+
+// Base URL for your domain
+const baseUrl = window.location.origin;
+
+// Regular expression for validating email addresses
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+// Function to join the wait list
+async function joinWaitList(email) {
+  const response = await fetch(`${baseUrl}/wait-list`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  });
+
+  if (response.ok) {
+    console.log('Successfully joined the waitlist');
+  } else {
+    console.error('Failed to join the waitlist');
+  }
+}
+
+joinWaitListEmailAddressEl.addEventListener('input', () => {
+  const { value } = joinWaitListEmailAddressEl;
+
+  if (value.trim() === '' || !emailRegExp.test(value.trim())) {
+    joinWaitListEmailAddressEl.setCustomValidity(
+      'Please input a valid email address',
+    );
+  } else {
+    joinWaitListEmailAddressEl.setCustomValidity('');
+  }
+
+  joinWaitListEmailAddressEl.reportValidity();
+});
+
+joinWaitListFormEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const emailAddressVal = joinWaitListEmailAddressEl.value.trim();
+
+  if (emailAddressVal === '' || !emailRegExp.test(emailAddressVal)) {
+    joinWaitListEmailAddressEl.setValidity(
+      'Please enter a valid email address',
+    );
+    joinWaitListEmailAddressEl.reportValidity();
+    return;
+  }
+
+  joinWaitList(emailAddressVal);
+});
+
 // Querying DOM elements for the off-screen menu toggle button and close button
 const toggleBtn = document.querySelector('.menu-toggle-btn');
 const offScreenMenu = document.querySelector('.off-screen-menu');
